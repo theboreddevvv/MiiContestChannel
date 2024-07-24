fabf3487cb85b6d08952e8a38a36a556b90fbad5
Larsenv <californiaduster+one@gmail.com>
2024-07-24 15:18:37 -0500

    fix top 50

--- plaza/top50.go
+++ plaza/top50.go
@@ -2,6 +2,7 @@ package plaza
 
 import (
 	"context"
+
 	"github.com/WiiLink24/MiiContestChannel/common"
 	"github.com/jackc/pgx/v4/pgxpool"
 )
@@ -10,7 +11,7 @@ const (
 	GetTop50Miis = `SELECT miis.entry_id, miis.initials, miis.perm_likes, miis.skill, miis.country_id, miis.mii_data, 
        			artisans.mii_data, artisans.artisan_id, artisans.is_master 
 				FROM miis, artisans WHERE miis.artisan_id = artisans.artisan_id 
-				ORDER BY miis.likes DESC LIMIT 50`
+				ORDER BY miis.perm_likes DESC LIMIT 50`
 )
 
 func MakeTop50(pool *pgxpool.Pool, ctx context.Context) error {

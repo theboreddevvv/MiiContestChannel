2fe76688ee8f2f8a3e8b4214fe4e5d8b45d9b580
theboreddevvv <californiaduster+one@gmail.com>
2024-07-24 14:55:48 -0500

    Update top50.go

--- plaza/top50.go
+++ plaza/top50.go
@@ -10,7 +10,7 @@ const (
 	GetTop50Miis = `SELECT miis.entry_id, miis.initials, miis.perm_likes, miis.skill, miis.country_id, miis.mii_data, 
        			artisans.mii_data, artisans.artisan_id, artisans.is_master 
 				FROM miis, artisans WHERE miis.artisan_id = artisans.artisan_id 
-				ORDER BY miis.likes DESC LIMIT 50`
+				ORDER BY miis.perm_likes DESC LIMIT 50`
 )
 
 func MakeTop50(pool *pgxpool.Pool, ctx context.Context) error {
